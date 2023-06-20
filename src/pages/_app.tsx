80f73331b034b4c9d71e47367ac10e63a7d48156
lucas-fer <alikacildes@gmail.com>
2023-06-20 19:45:38 -0300

    style: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
+import logoImg from '../assets/logo.svg'
+import Image from "next/image"
+import { Container, Header } from "../styles/pages/app"
 
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} width={130} height={52} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+
+
+  )
 }
 
 export default App
